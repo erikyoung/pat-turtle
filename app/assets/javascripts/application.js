d142d0e8f35f0ebc6b70520b82393b00dc3e1617
erikyoung <erik.young@rmit.edu.vn>
2016-09-25 00:44:57 +0700

    added quotes and changed font

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,9 @@
+//= require jquery
+//= require bootstrap-sprockets
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
